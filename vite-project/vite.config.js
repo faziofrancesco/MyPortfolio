cf97d76f4bc6be67ab8021849ab280ec7a82b0f9
fra_f <bankai979797@gmail.com>
2024-09-14 23:37:05 +0200

    first commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'build', // Assicurati che corrisponda al percorso di build atteso
+    rollupOptions: {
+      output: {
+        // Configurazioni addizionali di output, se necessario
+      }
+    }
+  },
 })
